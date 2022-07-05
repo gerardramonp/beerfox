8c7dd1c7087d4912ee001527e5063d6dbe9d8e3c
gerardramonp <gerardramonp@gmail.com>
2022-07-05 23:21:37 +0200

    Add sonarqube files to run scan

--- src/ui/DrawerMenu.test.tsx
+++ src/ui/DrawerMenu.test.tsx
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { testWrapperFactory } from "../utils/test-utils";
 import DrawerMenu from "./DrawerMenu";
 
@@ -6,5 +7,19 @@ describe("Given a DrawerMenu component", () => {
     const { findByText } = testWrapperFactory(<DrawerMenu />);
 
     await findByText("Settings");
+  });
+
+  describe("When user clicks on dark mode switch", () => {
+    test("Then should switch the app theme to dark theme", async () => {
+      const { findByTestId, findByText } = testWrapperFactory(<DrawerMenu />);
+
+      const dtSwitch = await findByTestId("dark-theme-switch");
+
+      await userEvent.click(dtSwitch);
+
+      const settingsHeader = await findByText("Settings");
+
+      expect(settingsHeader).toHaveStyle("background-color:asd");
+    });
   });
 });
